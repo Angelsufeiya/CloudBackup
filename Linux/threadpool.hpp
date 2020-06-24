29541efbd99924116668cdf9807c9aa769fe7fe3
武纪东 <Angelsufeiya@hotmail.com>
2020-06-24 22:48:01 +0800

    Linux

--- Linux/threadpool.hpp
+++ Linux/threadpool.hpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#define MAX_THREAD 5
+
 typedef void (*handler_t)(int);
 
 class ThreadTask(){
@@ -23,11 +25,10 @@ private:
 	handler_t _handler;
 };
 
-#define MAX_THREAD 10
 class ThreadPool{
 public:	
 	ThreadPool(int max_thr = MAX_THREAD):
-		_thr_max(thr_max)
+		_thr_max(max_thr)
 	{
 		pthread_mutex_init(&_mutex, NULL);
 		pthread_cond_init(&_cond, NULL);
@@ -58,8 +59,8 @@ public:
 		// 不断的从任务队列中取出任务，执行任务的Run接口就可以
 		while(1){
 			pthread_mutex_lock(&p->_mutex);
-			while(_queue.empty()){
-				pthread_cond_wait(&p->cond, &p->_mutex);
+			while(p->_queue.empty()){
+				pthread_cond_wait(&p->_cond, &p->_mutex);
 			}
 			ThreadTask task;
 			task = p->_queue.front();
