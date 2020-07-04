db0ddcee14386c5d42841a5ea22b83f357164b3d
武纪东 <Angelsufeiya@hotmail.com>
2020-07-04 23:20:57 +0800

    Linux

--- Linux/http_srv.cpp
+++ Linux/http_srv.cpp
@@ -1,6 +1,8 @@
 // 使用封装的TcpSocket类实例化对象实现tco服务端程序
 
 #include <iostream>
+#include <sstream>
+#include <stdlib.h>
 #include "tcpsocket.hpp"
 
 int main(int argc, char *argv[]){
@@ -19,8 +21,26 @@ int main(int argc, char *argv[]){
 		TcpSocket cli_sock;
 		bool ret = lst_sock.Accept(&cli_sock);
 		if(ret == false){
-			continue；
+			continue;
 		}
+
+		std::string http_req;
+		cli_sock.Recv(&http_req);
+		printf("req:[%s]\n", http_req.c_str());
+
+		// 响应-首行（版本/状态码/描述）-描述（Content-Length）-空行-正文
+		std::string body = "<html><body><h1>Hello, world!</h1></body></html>";
+		std::string blank = "\r\n";
+		std::stringstream header;
+		header << "Content - Length: " << body.size() << "\r\n";
+		header << "Content-Type: text/html\r\n";
+		std::string first_line = "HTTP/1.1 200 OK\r\n";
+
+		cli_sock.Send(first_line);
+		cli_sock.Send(header.str());
+		cli_sock.Send(blank);
+		cli_sock.Send(body); 
+		cli_sock.Close();
 	}
 	lst_sock.Close();
 	
